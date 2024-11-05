6101b04ec8b24bc34ec059f19f3e9501e21ab0f7
reza <rezasoltan12.1386@gmail.com>
2024-11-05 06:09:27 -0800

    lst commit

---
+++ filtering-rubika.py
@@ -0,0 +1,165 @@
+print("REZA")
+print(" **@Rezabkg1** ")
+print("  cityteam  ")
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time
+import sys
+import colored
+import colorama
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+os.system("clear")
+print(f"{red} ")
+print(Fore.WHITE + "")
+#date
+x = str(datetime.datetime.now())
+print("started " + (x))
+print(f"{green} ")
+print(" cod filtering ")
+print(Fore.RED + "")
+x = """ Loading ~ ~ ~
+
+                ..:::::::::..
+           ..:::aad8888888baa:::..
+        .::::d:?88888888888?::8b::::.
+      .:::d8888:?88888888??a888888b:::.
+    .:::d8888888a8888888aa8888888888b:::.
+   ::::dP::::::::88888888888::::::::Yb::::
+  ::::dP:::::::::Y888888888P:::::::::Yb::::
+ ::::d8:::::::::::Y8888888P:::::::::::8b::::
+.::::88::::::::::::Y88888P::::::::::::88::::.
+ :::::::Y88888888888P::|::Y88888888888P:::::::
+::::::::::::::::888:::|:::888::::::::::::::::
+`:::::::::::::::8888888888888b::::::::::::::'
+ :::::::::::::::88888888888888::::::::::::::
+  :::::::::::::d88888888888888:::::::::::::
+   ::::::::::::88::88::88:::88::::::::::::
+    `::::::::::88::88::88:::88::::::::::'
+      `::::::::88::88::88:::88::::::::'
+        `::::::88::88::88:::88::::::'
+           ``:::::::::::::::::::''
+                ``:::::::::::
+
+         ❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤
+         ❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤
+         🤍🤍🤍🤍🤍🤍🟡🟡🟡🤍🤍🤍🤍🤍
+         🤍🤍🤍🤍🤍🤍🟡🟡🟡🤍🤍🤍🤍🤍
+         💚💚💚💚💚💚💚💚💚💚💚💚💚💚
+         💚💚💚💚💚💚💚💚💚💚💚💚💚💚
+
+              C I T Y T E A M 
+
+
+"""
+for c in x:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.003)
+print(f"{blue} ")
+print("#.........")
+time.sleep(0.9)
+print("##........")
+time.sleep(0.9)
+print("###.......")
+time.sleep(0.8)
+print("####......")
+time.sleep(0.7)
+print("#####.....")
+time.sleep(0.6)
+print("######....")
+time.sleep(0.5)
+print("#######...")
+time.sleep(0.4)
+print("########..")
+time.sleep(0.3)
+print("#########.")
+time.sleep(0.2)
+print("##########")
+time.sleep(0.1)
+print(f"{pink} ")
+print("Play - - - ! ")
+print(Fore.YELLOW + "")
+time.sleep(1.7)
+print("  abar filtering (reza) ")
+print(" ")
+print("   chanel telegram = @city_team_ir  ")
+print(" ")
+print(f"{green} ")
+print(" cod server ")
+time.sleep(0.1)
+print("server1 = y//d/f")
+time.sleep(1.0)
+print("server2 = g/d//")
+time.sleep(0.1)
+print(" Dxprit ")
+time.sleep(1.0)
+print("Dxprit = http://Dxprit.nob.hackers6683648249.76.infect.in ")
+time.sleep(0.1)
+print(f"{red} ")
+up = input("""
+
+《 cod filter account 1 》
+
+《 cod filter gap 2 》
+
+《 cod filter chanel 3 》
+
+ Which do you want >>> """)
+print("  ")
+print(f"{blue} ")
+time.sleep(2.0)
+if up == "1":
+        print(' cod account = (4.9.3.1///u////y.7.7.4.1.1.5.7.0.8.6.4.2.2.6.7.2.fil.rubika.ir)')
+if up != "1":
+        print(' cod filter account ')
+if up == "2":
+        print(' cod gap = (fil.fil.ter.af.2.4.8.9.8.6.4.2.1.5.7.9.9.7.6.4.2/port:80.9.8.6.4/)')
+if up != "2":
+        print(' cod filter gap ')
+if up == "3":
+        print(' cod chanel = (report.fil.gif.sxs.com/(/yftt15k)filter.rubika.phone.sxs.com[CFC+BUG]) ')
+if up != "3":
+        print(' cod filter chanel ')
+time.sleep(0.6)
+print("""
+
+         cod haro mahklot konid !
+
+""")
+print(f"{pink} ")
+print(" cod ro 10 bar bezanid ke feik nashe ")
+print("♡Thanks♡")
+print(" ip rubika = 5.106.10.226 ")
+print(f"{red} ")
+print(" no love ")
+print(" ")
+print(" abar HACKER TEAM ( city-team.ir ) ")
+time.sleep(2.0)
+print(""" im alone (reza):
+    	
+    	
+creat by
+cityream
+
+
+""")
+print(" ")
+print(" ostore filtering ")
+print(" C I T Y T E A M ")
+print(f"{blue} ")
+print("phone number =bayd TOR bmonm  ") 
+print(f"{pink} ")
+print("the end ")
+time.sleep(2.0)
